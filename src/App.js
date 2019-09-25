6692d2d7387ca9d8801fc54364a0c19927c39a8c
Jessica <jessica.ho.jh@gmail.com>
2019-09-25 14:45:05 -0700

    shows users repos

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -59,6 +60,19 @@ class App extends Component {
     }
 
 
+    // Get users repos
+    getUserRepos = async username => {
+      this.setState({ loading: true });
+
+      const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=$
+      {process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=$
+      {process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+      
+      this.setState({ repos: res.data, loading: false });
+    }
+
+
     // Clear users from state
     clearUsers = () => this.setState({ users: [], loading: false});
   
@@ -95,7 +109,9 @@ class App extends Component {
             <User 
             {...props} 
             getUser={this.getUser} 
+            getUserRepos={this.getUserRepos}
             user={this.state.user} 
+            repos={this.state.repos}
             loading={this.state.loading}
             />
           )}
